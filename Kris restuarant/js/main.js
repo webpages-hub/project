decee8ffc8ecaf7981ce1704c86795a221ba5845
webpages-hub <63662796+webpages-hub@users.noreply.github.com>
2021-01-09 23:40:44 +0100

    Update main.js

--- Kris restuarant/js/main.js
+++ Kris restuarant/js/main.js
@@ -87,7 +87,7 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal(`.footer__content, .places, .reserve__container`, {
+sr.reveal(`.services__container, .footer__content, .places, .reserve__container`, {
     interval: 200
 });
 
