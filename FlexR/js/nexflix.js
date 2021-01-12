9a8c31a5f96d02d671d3846f83a8a178587e63c1
webpages-hub <63662796+webpages-hub@users.noreply.github.com>
2021-01-12 20:32:03 +0100

    Add files via upload

---
+++ FlexR/js/nexflix.js
@@ -0,0 +1,55 @@
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId)
+
+  if (toggle && nav){
+      toggle.addEventListener('click', ()=>{
+          nav.classList.toggle('show-menu')
+      })
+  }
+}
+showMenu('nav-toggle','nav-menu')
+
+/* REMOVE MENU MOBILE*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttributes('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+/* CHANGE BACKGROUND HEADER */
+function scrollHeader(){
+  const nav = document.getElementById('header')
+
+  if(this.scrollY >= 200) nav.classList.add('scroll-header');else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* SHOW SCROLL TOP */
+function scrollTop(){
+  const scrollTop = document.getElementById('scroll-top')
+
+  if(this.scrollY >= 560) scrollTop.classList.add('scroll-top');else scrollTop.classList.remove('scroll-top')
+}
+window.addEventListener('scroll', scrollTop)
+
+
