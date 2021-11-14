889e968f92f9475cd176dd5ef158acf4c51d65df
webpages-hub <63662796+webpages-hub@users.noreply.github.com>
2021-11-14 18:10:32 +0100

    Add files via upload

---
+++ About-Personal/components/About.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function About(){
+    return(
+        <section className= "about">
+        <h3>About</h3>
+        
+        <p>I am a Frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and I am always eager to learn new technology.</p>
+        
+        </section>
+    )
+}
---
+++ About-Personal/components/Interests.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Interests(){
+    return(
+        <section className="interest">
+        <h3>Interests</h3>
+          <p> Music. Reader. Food expert. Fashion geek. Entrepreneur. Realiity shows. Writing. Research. Culture.</p>
+     
+       </section>   
+    )
+}
